df7caa16a89cc411ba48d1ae945e25c3918cf662
Jonathan Werz <jdwerz83@gmail.com>
2021-12-19 13:53:28 -0500

    checkTimeBlock fucntion

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,21 @@ function clockUpdater(){
     timeEl.text(currentTime);
     checkTimeBlock();
 }
+
+function checkTimeBlock(){
+    var currentHour = currentTime.hours();
+    var timeBlock = $(".time-block");
+    for (let i = 0; i < timeBlock.length; i++) {
+        var block = timeBlock[i];
+        if (parseInt(block.id.split("-")[0]) < currentHour){
+            $(block).addClass("past");
+        }   else if (parseInt(block.id.split("-")[0]) === currentHour){
+                $(block).removeClass("past");
+                $(block).addClass("present");                
+        }   else{
+                $(block).removeClass("past");
+                $(block).removeClass("present");
+                $(block).addClass("future");
+        }        
+    }
+}
